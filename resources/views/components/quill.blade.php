de787ef793219d8c058ddff34dce7fe3dd52dc61
Hyn2 <justin010129@gmail.com>
2023-11-27 06:54:16 +0900

    feat: Bundle js files

--- resources/views/components/quill.blade.php
+++ resources/views/components/quill.blade.php
@@ -4,8 +4,5 @@
 
         <!-- Create the editor container -->
         <div id="editor" class="h-fit">
-
         </div>
-        <!-- Include the Quill library -->
-        <script defer src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 </div>
