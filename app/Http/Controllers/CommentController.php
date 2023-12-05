7cb98f0664584cfad2041752d386e14b163b1a4c
Hyn2 <justin010129@gmail.com>
2023-12-05 14:07:52 +0900

    #34 feat: Implement comment writing feature

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class CommentController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Post $post)
+    {
+        $request->validate([
+            'comment' => 'required|string',
+        ]);
+
+        Comment::create([
+            'post_id' => $post->id,
+            'user_id' => Auth::user()->id,
+            'comment' => $request->comment,
+        ]);
+
+        return Redirect::back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post, Comment $comment)
+    {
+        if($comment->exists) {
+            $comment->delete();
+        }
+        return Redirect::back();
+    }
+}
---
+++ resources/views/components/comment-box.blade.php
@@ -0,0 +1,13 @@
+@props(['post_id'])
+
+<div {{$attributes->merge(['class' => "m-1.5 flex flex-col w-[90%] border rounded-xl p-3"])}}>
+    <p class="text-xl font-semibold"><a href={{route('profile.show', ['user'=>$comment->user_id])}}>{{$comment->user->name}}</a></p>
+    <p class="ml-1.5 mt-1.5 font-thin">{{$comment->comment}}</p>
+    @if(Auth::user()->id == $comment->user_id)
+        <form method="post" action={{route('comment.destroy', ['post'=>$post_id, 'comment' => $comment->id])}}>
+            @csrf
+            @method('DELETE')
+            <x-primary-button class="float-right" onclick="confirm('정말 삭제하시겠습니까?')">삭제</x-primary-button>
+        </form>
+    @endif
+</div>
