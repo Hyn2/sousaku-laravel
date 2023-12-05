6a531c0ddc49130f53b5fc9de1741bf99195a654
Hyn2 <justin010129@gmail.com>
2023-12-06 01:08:04 +0900

    fix: Resolve issue with value initialization

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -8,9 +8,11 @@
                     <x-primary-button id="formSubmit">작성</x-primary-button>
                 </div>
                     <div class="my-1 items-center px-8">
-                        <x-input-label class="w-fit block mb-1" for="title" :value="__('제목')"/>
-                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
-                        <x-text-input class="w-full my-1" id="title" name="title" type="text" placeholder="TITLE"/>
+                        <div class="flex items-center">
+                            <x-input-label class="w-fit mr-3" for="title" :value="__('제목')"/>
+                            <x-input-error :messages="$errors->get('title')" />
+                        </div>
+                        <x-text-input class="w-full my-1" id="title" name="title" :value="old('title', '')" type="text" placeholder="TITLE"/>
                     </div>
                 <div class="my-1">
                     <div class="flex justify-evenly">
@@ -19,32 +21,36 @@
                     </div>
                     <div class="flex justify-evenly">
                         <x-select name="gender">
-                            <option value="N">무관</option>
-                            <option value="M">남성</option>
-                            <option value="F">여성</option>
+                            <option value="N" {{ old('gender') == "N" ? 'selected' : '' }}>무관</option>
+                            <option value="M" {{ old('gender') == "M" ? 'selected' : '' }}>남성</option>
+                            <option value="F" {{ old('gender') == "F" ? 'selected' : '' }}>여성</option>
                         </x-select>
                         <x-select name="region">
                             @foreach($regions as $value)
-                                <option value={{$value->id}}>{{$value->region}}</option>
+                                <option value={{$value->id}} {{ old('region') == $value->id ? 'selected' : '' }}>{{$value->region}}</option>
                             @endforeach
                         </x-select>
                         <x-select class="hidden" id="positions" name="positions[]" multiple="true">
                             @foreach($positions as $value)
-                                <option id={{$value->id}} value={{$value->id}}></option>
+                                <option id={{$value->id}} value={{$value->id}} {{ in_array($value->id, old('positions', [])) ? 'selected' : '' }}></option>
                             @endforeach
                         </x-select>
                     </div>
-                    <p class="text-sm px-8">{{__('포지션')}}</p>
-                    <x-input-error :messages="$errors->get('positions')" class="mt-2" />
+                    <div class="flex items-center mt-3">
+                        <p class="text-sm mx-3">{{__('포지션')}}</p>
+                        <x-input-error :messages="$errors->get('positions')" />
+                    </div>
                     <div class="mt-4 mb-5 flex flex-wrap px-7">
                         @foreach($positions as $value)
-                            <x-tag class="mx-3 hover:scale-105" :value="$value->id">{{$value->position}}</x-tag>
+                            <x-tag class="mx-3 hover:scale-105 {{ in_array($value->id, old('positions', [])) ? 'clicked' : '' }}" :value="$value->id">{{$value->position}}</x-tag>
                         @endforeach
                     </div>
                 </div>
-                <x-input-label class="w-3/12 mx-8" for="image" :value="__('이미지')" />
-                <x-input-error :messages="$errors->get('image')" class="mt-2" />
-                <div id="imageContainer" class="flex items-center w-auto h-20 px-8">
+                <div class="flex items-center">
+                    <x-input-label class="w-fit mx-3" for="image" :value="__('이미지')" />
+                    <x-input-error :messages="$errors->get('image')" />
+                </div>
+                <div id="imageContainer" class="flex items-center w-auto h-16 px-8">
                     <label for="image" class="mr-5">
                         <div class="border px-3 py-2 rounded-2xl hover:scale-105 font-thin">사진 업로드하기</div>
                     </label>
@@ -56,7 +62,7 @@
                 <hr />
                 <x-input-error :messages="$errors->get('htmlContent')" class="mt-2" />
                 <x-quill></x-quill>
-                <x-text-input hidden type="text" id="htmlContent" name="htmlContent"/>
+                <x-text-input hidden type="text" id="htmlContent" :value="old('htmlContent', '')" name="htmlContent"/>
             </form>
         </div>
     </div>
