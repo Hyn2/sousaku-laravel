4a16b7a9e7cfb2ce6ed0ea2edc4a5c03e3d5cf05
Hyn2 <justin010129@gmail.com>
2023-12-06 14:29:06 +0900

    design: Add responsive design in iframe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,13 @@
         @if(auth()->user()->admin ?? false)
             <button class="text-center mt-3 text-gray-300 hover:text-gray-600">Carousel 수정하기</button>
         @endif
-        <div class="flex justify-center w-full h-[800px]">
+        <div class="flex justify-center w-full h-fit">
             <div id="prevDiv" class="flex items-center mr-3">
-                <button id="prev" class="text-5xl"><</button>
+                <button id="prev" class="text-5xl h-full"><</button>
             </div>
             <div id="carousel" class="mt-10 basis-2/3 overflow-hidden">
-                <div id="container" class="w-full h-full flex pb-10">
-                    <iframe class="h-auto min-w-full"
+                <div id="container" class="w-full pb-[56.25%] relative flex pb-10">
+                    <iframe class="absolute w-full h-full"
                             title="YouTube video player"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             allowfullscreen
@@ -36,7 +36,7 @@
                 </div>
             </div>
             <div id="nextDiv" class="flex items-center ml-3">
-                <button id="next" class="text-5xl">></button>
+                <button id="next" class="text-5xl h-full">></button>
             </div>
         </div>
     </div>
