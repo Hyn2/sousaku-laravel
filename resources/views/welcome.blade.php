a269f17237d556dc13fd76066164887642fcd700
Hyn2 <justin010129@gmail.com>
2023-12-06 20:56:38 +0900

    feat: Add exception code for missing video link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 <x-app-layout>
     <div class="flex flex-col items-center my-20 gap-32">
         <div id="recentPost" class="grid gap-3 w-[80%]">
-            <h1 class="text-3xl font-thin text-center pb-4 border-b w-full">
+            <h2 class="text-3xl font-thin text-center pb-4 border-b w-full">
                 최근 공고
-            </h1>
+            </h2>
             <x-post-box :posts="$posts"></x-post-box>
         </div>
         <div id="weeklyMusic" class="grid gap-3 w-[80%]">
@@ -21,12 +21,18 @@
             </div>
             @endif
             <div class="mt-10 relative pb-[56.25%]">
+            @if($embedLink->link=='')
+                <div class="w-full h-full absolute flex justify-center items-center bg-gray-50 rounded-xl ">
+                    <p class="text-5xl font-thin text-center pb-4 w-full my-10">아직 등록된 음악이 없어요.</p>
+                </div>
+            @else
                 <iframe class="w-full absolute h-full"
                         title="YouTube video player"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         allowfullscreen
                         src={{$embedLink->link}}>
                 </iframe>
+            @endif
             </div>
         </div>
     </div>
